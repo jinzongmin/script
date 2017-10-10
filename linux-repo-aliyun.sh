7956b997c9c6d5552c4cd82cf53a14b9d21cb34f
jinzongmin <jinzongmin@hotmail.com>
2017-10-10 18:41:31 +0800

    jin

--- linux-repo-aliyun.sh
+++ linux-repo-aliyun.sh
@@ -1,37 +1,46 @@
-### Replace repo to aliyun
+### 替换阿里云Repo
+
+# 配置
+BEGIN_TS=`date +%s`
+REPO_FILE_PATH="/etc/yum.repos.d/CentOS-Base.repo"
+REPO_FILE_BACKUP="/etc/yum.repos.d/CentOS-Base.repo.backup_`date -d @$BEGIN_TS '+%Y%m%d_%H%M%S'`"
 
 # 检查系统信息 [http://zhjwpku.com/2016/11/30/os-identification.html,https://unix.stackexchange.com/questions/111508/bash-test-if-word-is-in-set]
 source /etc/os-release
 
-if [[ "$ID" =~ ^(centos)$ ]]; then
-    #支持的操作系统
-else
-	#不支持的操作系统
-    echo "不支持的操作系统:$ID"
-fi
-
+SUCCESS=false
 case $ID in
-    centos) echo yes;;
-    *)             echo no;;
-esac
-
-case $ID in
-	centos ) 
+	centos)
 		case $VERSION_ID in
-			6 )
-				
+			5|6|7)
+				# 备份本地Repo
+				echo -n "备份Repo ... "
+				mv $REPO_FILE_PATH $REPO_FILE_BACKUP \
+					&& echo "成功" \
+					|| (echo "失败" && exit)
+				# 下载阿里云Repo
+				REPO_URL="http://mirrors.aliyun.com/repo/Centos-$VERSION_ID.repo"
+				echo -n "下载Repo ... "
+				wget -q -O $REPO_FILE_PATH $REPO_URL \
+					&& echo "成功" && SUCCESS=true \
+					|| (echo "失败，URL=$REPO_URL" && mv $REPO_FILE_BACKUP $REPO_FILE_PATH && exit)
 				;;
-			* )
+			*)
 				echo "不支持的操作系统版本:$ID $VERSION_ID"
+				;;
 		esac
-	* )	
+		;;
+	*)
 		echo "不支持的操作系统:$ID"
-		exit
+		;;
 esac
 
-exit
-
-# 备份
-mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup_`date '+%Y%m%d_%H%M%S'`
-
-#
+if $SUCCESS ; then
+    echo "刷新缓存 ..."
+    yum clean all
+	yum makecache
+	echo "\n"
+	echo "**** Repo更新完毕 ***\n"
+else
+	echo "所有变更已回滚"
+fi
